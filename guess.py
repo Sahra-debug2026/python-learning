539be61d1fe994c049468b6e4ef8e1e27392d6dc
Sahra-debug2026 <zahrahosseini822@gmail.com>
2026-06-17 16:04:33 +0330

    final cleanup project structure

--- guess.py
+++
@@ -1,9 +0,0 @@
-import random
-number=random.randint(1,10)
-
-guess=int(input("what is your guess?")) 
-
-if guess == number:
-    print("Congratulations! You guessed it!")
-else:
-    print(f"sorry, try again. the number was {number}.")    
